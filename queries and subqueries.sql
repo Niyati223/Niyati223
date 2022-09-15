a6b1cea50e5b238de4fa692a4977f30e152542c3
Niyati223 <112920390+Niyati223@users.noreply.github.com>
2022-09-15 09:15:04 +0530

    Add files via upload

---
+++ queries and subqueries.sql
@@ -0,0 +1,52 @@
+with A 
+as
+(
+select PurchaseOrderID,VendorID,OrderDate,TaxAmt,Freight,TotalDue,
+Most_Expensive_Rank= ROW_NUMBER()over(partition by VendorID order by TotalDue desc)
+from Purchasing.PurchaseOrderHeader
+)
+
+select PurchaseOrderID,VendorID,OrderDate,TaxAmt,Freight,TotalDue,Most_Expensive_Rank
+from A
+where Most_Expensive_Rank<=3 
+
+with b
+as
+(
+select PurchaseOrderID,VendorID,OrderDate,TaxAmt,Freight,TotalDue,
+Most_Expensive_Rank= ROW_NUMBER()over(partition by VendorID order by TotalDue desc),
+Most_Expensive_Ranking= row_number()over(order by TotalDue desc)
+from Purchasing.PurchaseOrderHeader
+)
+
+select PurchaseOrderID,VendorID,OrderDate,TaxAmt,Freight,TotalDue
+from b
+where Most_Expensive_Rank<=3
+
+--scalar subqueries
+select BusinessEntityID,JobTitle,VacationHours
+from HumanResources.Employee;
+
+
+select BusinessEntityID,JobTitle,VacationHours,
+MaxVacationHours=Max(VacationHours)
+from HumanResources.Employee
+ 
+select BusinessEntityID,JobTitle ,VacationHours,
+VacationHours * 100.00/(select max(VacationHours)from HumanResources.Employee)as 'Percentage'
+from HumanResources.Employee
+order by VacationHours desc
+
+select BusinessEntityID,JobTitle ,VacationHours,
+VacationHours * 100.00/(select max(VacationHours)
+from HumanResources.Employee)as 'Percentage'
+from HumanResources.Employee
+where VacationHours >80
+order by VacationHours desc
+
+
+
+
+
+
+
