a0ed5727b52d1c3df0a4d0c11237ad80b16c6690
Niyati223 <112920390+Niyati223@users.noreply.github.com>
2022-09-19 15:49:28 +0530

    Add files via upload

---
+++ divisbleby5.py
@@ -0,0 +1,7 @@
+num=[10,20,46,55,33]
+print('givenlist:',num)
+print('divisible by 5:')
+for i in num:
+  if i%5==0:
+    print(i)
+
---
+++ stringeven.py
@@ -0,0 +1,6 @@
+word = 'pynative'
+print("Original String:", word)
+x = list(word)
+for i in x[0::2]:
+    print(i)
+
